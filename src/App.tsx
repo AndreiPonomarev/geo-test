75459c52f5401fc5ee7f5155f37a77c8c660fe81
Andrei Ponomarev <bukinator.b@gmail.com>
2023-03-06 10:27:56 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+interface Coords {
+  latitude: number;
+  longitude: number;
+}
+interface Position {
+  coords: Coords
+}
+
+
+
 function App() {
+  const watchID = navigator.geolocation.watchPosition(showPosition);
+  const [position, setPosition] = useState<Coords>()
+  const [err, setErr] = useState()
+  function showPosition(position: Position) {
+    const {latitude, longitude} = position.coords;
+    console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+    setPosition({latitude, longitude})
+  }
+  
+  const errHandler = (e: any) => {
+    // alert(e)
+    setErr(e)
+  }
+  
+  useEffect(()=> {
+
+    if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showPosition, errHandler);
+} else {
+  alert("Geolocation is not supported by this browser.");
+}
+      setTimeout(function() {
+        navigator.geolocation.clearWatch(watchID);
+      }, 1000);
+
+  }, [watchID])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Coords: 
+      <pre>{JSON.stringify(position, null, 2)}</pre>
+      Err: 
+      <pre>{JSON.stringify(err, null, 2)}</pre>
     </div>
   );
 }
